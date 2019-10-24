dad10ea0b6a8fd1e414d36f89a46a290329f5ab5
Alex <Alex@swuser-129-8-236-51.swlan.csufresno.edu>
2019-10-24 10:46:35 -0700

    Added camera plugin, working on getting page set up

---
+++ PictureThis/PictureThis/View/CameraPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PictureThis.View
+{
+    public partial class CameraPage : ContentPage
+    {
+        public CameraPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
