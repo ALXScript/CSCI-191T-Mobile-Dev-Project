7cf88c585afaf3dd952177afb281251821abf53a
Steven Ryan Ramos <43050949+rsteven46@users.noreply.github.com>
2019-10-25 09:45:41 -0700

    binding

--- PictureThis/PictureThis/View/LocalImages.xaml.cs
+++ PictureThis/PictureThis/View/LocalImages.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,6 +12,7 @@ namespace PictureThis.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LocalImages : ContentPage
     {
+        public string dir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyPictures);
         public LocalImages()
         {
             InitializeComponent();
