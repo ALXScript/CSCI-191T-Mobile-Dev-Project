f181a3db701b3a0050e6506fcbce3fbab4812ca8
Alex <Alex@Alexs-MacBook-Pro-10.local>
2019-10-28 23:26:35 -0700

    Camera works...
    on iOS, still need to test on android. Once it works on android, going to test saving to device through an optional field editor for labeling and what not

--- PictureThis/PictureThis/View/CameraPage.xaml.cs
+++ PictureThis/PictureThis/View/CameraPage.xaml.cs
@@ -10,6 +10,15 @@ namespace PictureThis.View
         public CameraPage()
         {
             InitializeComponent();
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+            if (photo != null)
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
         }
     }
 }
