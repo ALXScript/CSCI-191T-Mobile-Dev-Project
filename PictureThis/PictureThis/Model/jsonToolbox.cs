bfd47c94129d979c679bbaa1b6ef434812d7f645
Alex <Alex@swuser-129-8-238-61.swlan.csufresno.edu>
2019-11-06 15:26:15 -0800

    updated NuGet package and working on JSON Toolbox

---
+++ PictureThis/PictureThis/Model/jsonToolbox.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PictureThis.Model
+{
+    public class JSONToolbox
+    {
+        string tagsPath = "Resources/tags.json";
+        string imagesPath = ""; //Get this later
+
+        public JSONToolbox()
+        {
+
+        }
+
+        void addTag(string newTag)
+        {
+            //convert the json to a string
+            string jsonString = System.IO.File.ReadAllText(tagsPath);
+
+            //init empty list of tags
+            List<string> tagsList;
+
+            //deserialize json into list of tags
+            tagsList = JsonConvert.DeserializeObject<List<string>>(jsonString);
+
+            //add a new tag
+            tagsList.Add(newTag);
+
+            //sort the list
+            tagsList.Sort();
+
+            //serialize list back into json
+            jsonString = JsonConvert.SerializeObject(tagsList);
+
+            //write back to the new json file
+            System.IO.File.WriteAllText(tagsPath, jsonString);
+
+        }
+
+        public void removeTag(string currentTag)
+        {
+            //convert the json to a string
+            string jsonString = System.IO.File.ReadAllText(tagsPath);
+
+            //init empty list of tags
+            List<string> tagsList;
+
+            //deserialize json into list of tags
+            tagsList = JsonConvert.DeserializeObject<List<string>>(jsonString);
+
+            //check if the tag is in the list
+            if (tagsList.IndexOf(currentTag) != -1)
+            {
+                tagsList.Remove(currentTag);
+            }
+
+            //sort the list
+            tagsList.Sort();
+
+            //serialize list back into json
+            jsonString = JsonConvert.SerializeObject(tagsList);
+
+            //write back to the new json file
+            System.IO.File.WriteAllText(tagsPath, jsonString);
+        }
+    }
+}
--- PictureThis/PictureThis/View/CameraPage.xaml.cs
+++ PictureThis/PictureThis/View/CameraPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Essentials;
 
 namespace PictureThis.View
 {
@@ -19,6 +20,65 @@ namespace PictureThis.View
 
             if (photo != null)
                 PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+        }
+
+        async void setupFillData()
+        {
+            //Declare the Variables
+            string imageName;
+            DateTime imageDateTime;
+            Location imageLocation;
+            bool imageLike = false;
+            string[] imageTags;
+
+            //"Invisible" Operations: Set like status (discrete & not viewable) and get location (discrete but Viewable)
+            //Like status operation already performed
+            //Get the GeoLocation in Variables
+            try
+            {
+                var geoRequest = new GeolocationRequest(GeolocationAccuracy.Medium);
+                var geoLocation = await Geolocation.GetLocationAsync(geoRequest);
+
+                //if it's not null we got your location
+                if(geoLocation != null)
+                {
+                    imageLocation = geoLocation;
+                }
+                
+            }
+            //Else we don't have your location and we are substituting with temporary locations
+            catch (FeatureNotSupportedException fnsEX)
+            {
+                //Not supported on device, substitute
+                imageLocation = Geolocation.;
+            }
+            catch (FeatureNotEnabledException fneEX)
+            {
+                //Not enabled on device, substitute
+            }
+            catch (PermissionException pEX)
+            {
+                //Permission exception, substitute
+            }
+            catch (Exception ex)
+            {
+                //couldn't get location, substitute
+            }
+
+            //Add field for setting the image name
+            var entCellName = new EntryCell { Label = "Image Name: " };
+
+            //Add Fields for Date and Time (read only)
+            imageDateTime = DateTime.Today;
+            var entCellDateTime = new EntryCell { Label = imageDateTime.ToString(), IsEnabled=false};
+
+            //Add Fields for Location
+            var entCellLocation = new EntryCell { Label= imageLocation.ToString(), IsEnabled=false };
+
+            //Add Tags Field
+            var entCellTags = new EntryCell { Label = "Tags: " };
+            
+            
         }
     }
 }
