ffd4e515f354435a35d9440e7f75dfc102826621
Steven Ryan Ramos <43050949+rsteven46@users.noreply.github.com>
2019-12-09 09:59:00 -0800

    Added top5rated pictures
    added page for top 5 rated pictures

--- PictureThis/PictureThis/View/GalleryPage.xaml.cs
+++ PictureThis/PictureThis/View/GalleryPage.xaml.cs
@@ -16,5 +16,6 @@ namespace PictureThis
             //DisplayAlert("Reset", "File has been Reset", "OK");
         }
         protected async void OnBrowseClickedLocation(object sender, EventArgs e) => await Navigation.PushAsync(new LocationPage());
+        protected async void OnBrowseClickedTop5Rated(object sender, EventArgs e) => await Navigation.PushAsync(new top5Ratedpage());
     }
 }
